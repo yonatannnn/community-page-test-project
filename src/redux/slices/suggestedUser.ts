03ad3d2318d2a2ec986fb75ffb183265af8de964
yonathan <yonatanalebachew7@gmail.com>
2025-01-22 08:53:21 +0300

    add suggested user slice for state management. suggestedUser.ts

---
+++ src/redux/slices/suggestedUser.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import User from '../../models/user';
+import { suggestedUsers } from '@/utils/SuggestedUser';
+
+const suggestedUsersSlice = createSlice({
+    name: 'suggestedUsers',
+    initialState: suggestedUsers,
+    reducers: {
+        setSuggestedUsers: (_, action: PayloadAction<User[]>) => {
+            return action.payload;
+        },
+        addSuggestedUser: (state, action: PayloadAction<User>) => {
+            state.push(action.payload);
+        },
+        removeSuggestedUser: (state, action: PayloadAction<string>) => {
+            return state.filter(user => user.username !== action.payload);
+        },
+        clearSuggestedUsers: () => {
+            return [];
+        },
+    },
+});
+
+export const {
+    setSuggestedUsers,
+    addSuggestedUser,
+    removeSuggestedUser,
+    clearSuggestedUsers,
+} = suggestedUsersSlice.actions;
+
+export default suggestedUsersSlice.reducer;
